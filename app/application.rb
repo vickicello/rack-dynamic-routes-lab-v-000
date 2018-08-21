5b0775994cedbab99825f4cc27802c227331e2b2
Vicki Hamm <vicki.cello@gmail.com>
2018-08-21 00:22:10 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  @@items = []
+ 
+ def call(env)
+   resp = Rack::Response.new
+   req = Rack::Request.new(env)
+ 
+   if req.path=="/songs"
+     resp.write "You requested the songs"
+   else
+     resp.write "Route not found"
+     resp.status = 404
+   end
+ 
+   resp.finish
+ end
+end
