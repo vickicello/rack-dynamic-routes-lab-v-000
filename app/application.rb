559719f1eeaab0e9a2e44c66e83f4999bb61a4e9
Vicki Hamm <vicki.cello@gmail.com>
2018-08-21 00:22:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
    if req.path=="/items"
      resp.write "You requested the songs"
    else
-     resp.write "Item not found"
+     resp.write "Route not found"
      resp.status = 404
    end
 
