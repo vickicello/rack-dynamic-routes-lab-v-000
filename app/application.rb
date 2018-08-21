828d9dea6816b75a928df9d785e8496decd4b69f
Vicki Hamm <vicki.cello@gmail.com>
2018-08-21 00:29:23 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      new_item = req.path.split("/items/").last
+      item = @@items.find { |i| i.name == new_item }
+
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+
+    else
+     resp.write "Route not found"
+     resp.status = 404
+    end
+
+   resp.finish
+  end
+end
