e75c11a3307eee2455e36a236fdb2f948b4389bf
Vicki Hamm <vicki.cello@gmail.com>
2018-08-21 00:22:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,18 @@
 class Application
 
   @@items = []
- 
+
  def call(env)
    resp = Rack::Response.new
    req = Rack::Request.new(env)
- 
-   if req.path=="/songs"
+
+   if req.path=="/items"
      resp.write "You requested the songs"
    else
      resp.write "Route not found"
      resp.status = 404
    end
- 
+
    resp.finish
  end
 end
